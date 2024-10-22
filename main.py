ce70a9b7fa845f256e643fff1f6146b1603f6b38
nholert <110404845+nholert@users.noreply.github.com>
2024-10-22 06:26:03 -0700

    Update main.py - logging line 192

--- main.py
+++ main.py
@@ -189,6 +189,7 @@ class User(UserMixin):
         session['finish'] = f"https://spectrumsurveys.com/surveydone?st=21&transaction_id={session['token']}"
         session['quality'] = f"https://spectrumsurveys.com/surveydone?st=20&transaction_id={session['token']}"
         session['dedupe'] = f"https://spectrumsurveys.com/surveydone?st=30&transaction_id={session['token']}"
+        logging.error(f"Generated Finish Redirect URL: {session['finish']}")
         logged_in = User.login_user(token)
         if logged_in:
             return redirect('/')
