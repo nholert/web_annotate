0251aff216757e6dad1d12c38bac257c0e1341e5
nholert <110404845+nholert@users.noreply.github.com>
2024-11-18 22:38:55 -0800

    Update summary.py

--- summary.py
+++ summary.py
@@ -36,5 +36,6 @@ csrf = CSRFProtect(app)
 
 survey = json.load(open('survey.json'))
 
-if __name__=="__main__":
-    app.run(host='0.0.0.0',port='8976',debug=True)
+if __name__ == "__main__":
+    from waitress import serve  
+    serve(app, host="0.0.0.0", port=8976)
